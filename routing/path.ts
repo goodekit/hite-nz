0608c65c55e5da2bac8dbcbc7c878e1f00304d4e
bally <bally.lomibao@howickltd.com>
2024-05-11 22:58:58 +1200

    🎉 init(Init: Add routing functionality and path constants):

---
+++ routing/path.ts
@@ -0,0 +1,22 @@
+export const PATH_SIDEBAR = [
+  {
+    imgURL: '/icons/home.svg',
+    route: '/',
+    label: 'Home',
+  },
+  {
+    imgURL: '/icons/dollar-circle.svg',
+    route: '/my-banks',
+    label: 'My Banks',
+  },
+  {
+    imgURL: '/icons/transaction.svg',
+    route: '/transaction-history',
+    label: 'Transaction History',
+  },
+  {
+    imgURL: '/icons/money-send.svg',
+    route: '/payment-transfer',
+    label: 'Transfer Funds',
+  },
+]
