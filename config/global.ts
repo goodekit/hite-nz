ad94309680c4a912b4dc4aef0aa1ee2b0e97a969
bally <bally.lomibao@howickltd.com>
2024-05-11 22:56:44 +1200

    Init: [Config] Update constant/index.ts with test user and access token data

--- config/global.ts
+++ config/global.ts
@@ -16,3 +16,10 @@ export const ITEMS = [
     accountId: 'x1GQb1lDrDHWX4BwkqQbI4qpQP1lL6tJ3VVo9',
   },
 ]
+
+// good_user / good_password - Bank of America
+export const TEST_USER_ID = '6627ed3d00267aa6fa3e'
+
+// custom_user -> Chase Bank
+// export const TEST_ACCESS_TOKEN =
+//   "access-sandbox-da44dac8-7d31-4f66-ab36-2238d63a3017";
