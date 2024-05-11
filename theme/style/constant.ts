20fa3b13ea7f3810cb878cf67618f9743a935288
bally <bally.lomibao@howickltd.com>
2024-05-11 22:59:31 +1200

    Init: [Constant] Add transaction category styles and constants
    This commit adds transaction category styles and constants to the `constant.ts` file in the `theme/style` directory. The styles and constants define the appearance and behavior of different transaction categories in the application. This enhancement improves the functionality and visual consistency of the application's transaction categories.

---
+++ theme/style/constant.ts
@@ -0,0 +1,41 @@
+export const topCategoryStyles = {
+  'Food and Drink': {
+    bg: 'bg-blue-25',
+    circleBg: 'bg-blue-100',
+    text: {
+      main: 'text-blue-900',
+      count: 'text-blue-700',
+    },
+    progress: {
+      bg: 'bg-blue-100',
+      indicator: 'bg-blue-700',
+    },
+    icon: '/icons/monitor.svg',
+  },
+  Travel: {
+    bg: 'bg-success-25',
+    circleBg: 'bg-success-100',
+    text: {
+      main: 'text-success-900',
+      count: 'text-success-700',
+    },
+    progress: {
+      bg: 'bg-success-100',
+      indicator: 'bg-success-700',
+    },
+    icon: '/icons/coins.svg',
+  },
+  default: {
+    bg: 'bg-pink-25',
+    circleBg: 'bg-pink-100',
+    text: {
+      main: 'text-pink-900',
+      count: 'text-pink-700',
+    },
+    progress: {
+      bg: 'bg-pink-100',
+      indicator: 'bg-pink-700',
+    },
+    icon: '/icons/shopping-bag.svg',
+  },
+}
