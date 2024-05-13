0d9fb5db26443c78db4846954b12488a5fee2722
bally <bally.lomibao@howickltd.com>
2024-05-13 23:37:56 +1200

    Add AnimateCounter component for animated number display

---
+++ component/animate/counter.tsx
@@ -0,0 +1,20 @@
+'use client'
+import CountUp from 'react-countup'
+import { CURRENCY } from 'constant'
+
+const AnimateCounter = ({ amount }: { amount: number }) => {
+  const setting = {
+    decimals: 2,
+    decimal: ',',
+    prefix: CURRENCY.NZD,
+    end: amount,
+  }
+
+  return (
+    <div className='w-full'>
+      <CountUp {...setting} />
+    </div>
+  )
+}
+
+export default AnimateCounter
