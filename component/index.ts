5cc4a3d98e9facdf0ab1f120f9346f6d94f4a865
bally <bally.lomibao@howickltd.com>
2024-05-13 23:39:14 +1200

    Add component exports and update imports

---
+++ component/index.ts
@@ -0,0 +1,6 @@
+export * from './header'
+export * from './ui/button'
+
+export { default as AnimateCounter } from './animate/counter'
+export { default as BalanceBox } from './balance/balance-box'
+export { default as HeaderBox } from './header/header-box'
