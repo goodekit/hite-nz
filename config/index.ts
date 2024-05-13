170a42629ca7013c13f5152c48f8c253d037e646
bally <bally.lomibao@howickltd.com>
2024-05-14 00:54:45 +1200

    Add nav-link configuration
    This commit adds the nav-link configuration to the repository. The nav-link module is implemented in the `config/nav-link.ts` file and provides an array of navigation links with their respective image URLs, routes, and labels. This addition enhances the user interface by enabling the display of a navigation menu with icons and labels for easy access to different sections of the application.

--- config/index.ts
+++ config/index.ts
@@ -1,2 +1,3 @@
 export * from './asset-dir'
 export * from './global'
+export * from './nav-link'
---
+++ config/nav-link.ts
@@ -0,0 +1,27 @@
+export const navLink = [
+  {
+    imgUrl: '/asset/svg/home.svg',
+    route: '/',
+    label: 'Home',
+  },
+  {
+    imgUrl: '/asset/svg/bank-transfer.svg',
+    route: '/my-account',
+    label: 'My Account',
+  },
+  {
+    imgUrl: '/asset/svg/transaction.svg',
+    route: '/transaction',
+    label: 'Transaction History',
+  },
+  {
+    imgUrl: '/asset/svg/connect-bank.svg',
+    route: '/setting',
+    label: 'Setting',
+  },
+  {
+    imgUrl: '/asset/svg/money-send.svg',
+    route: '/transfer',
+    label: 'Transfer',
+  },
+]
