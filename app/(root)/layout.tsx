848bd3bf389ad290105ff02ab3a26e5988339bc0
bally <bally.lomibao@howickltd.com>
2024-05-14 00:53:50 +1200

    Add Sidebar component to RootLayout
    This commit adds the Sidebar component to the RootLayout in the layout.tsx file. The Sidebar component is imported from the 'component' module and is rendered within the main container. This addition enhances the user interface by providing a sidebar navigation menu for easy access to different sections of the application.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,17 @@
+import { Sidebar } from 'component'
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const loggedIn = {
+    firstname: 'Joe',
+    lastname: 'Doe',
+  }
   return (
-    <main>
-      SIDEBAR
+    <main className='flex h-screen w-full font-inter'>
+      <Sidebar />
       {children}
     </main>
   )
