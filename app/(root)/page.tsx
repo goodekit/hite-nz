096a9667e16d25805225c2cd5e73552b042b5df6
bally <bally.lomibao@howickltd.com>
2024-05-13 23:37:43 +1200

    Improve Home page layout and add user greeting
    This commit improves the layout of the Home page by adding a section with a user greeting. It also introduces a `HeaderBox` component to display a welcome message and the user's first name. These changes enhance the user experience and provide a personalized touch to the Home page.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { BalanceBox, HeaderBox } from 'component'
 
 const Home = () => {
-  return <div>Home</div>
+  const user = {
+    firstname: 'Joe',
+    lastname: 'Doe',
+    email: 'john.doe@example.com',
+  }
+
+  return (
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox type='greeting' title='Welcome' user={user.firstname || 'Guest'} subtext='this is a description' />
+          <BalanceBox account={[]} bank={1} totalCurrentBalance={2356} />
+        </header>
+      </div>
+    </section>
+  )
 }
 
 export default Home
