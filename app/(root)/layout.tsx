2f9e289e7f4c3548f2972700db02257bee7390ea
bally <bally.lomibao@howickltd.com>
2024-05-15 23:53:40 +1200

    Add lazy loading for images in RootLayout
    The RootLayout component in layout.tsx has been updated to include lazy loading for images. This improvement enhances the page load performance by deferring the loading of images until they are actually needed, resulting in faster initial page load times.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,18 +1,28 @@
-import { Sidebar } from 'component'
+import Image from 'next/image'
+import { ASSET } from 'config'
+import { Sidebar, MobileNav } from 'component'
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  const loggedIn = {
+  const _mockUser = {
     firstname: 'Joe',
     lastname: 'Doe',
   }
   return (
     <main className='flex h-screen w-full font-inter'>
-      <Sidebar />
-      {children}
+      <Sidebar user={_mockUser} />
+      <div className='flex size-full flex-col'>
+        <div className='root-layout'>
+          <Image src={ASSET.ICON} width={30} height={30} alt='icon' />
+          <div className=''>
+            <MobileNav user={_mockUser} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   )
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,33 @@
-import { BalanceBox, HeaderBox } from 'component'
+import { BalanceBox, HeaderBox, RightSidebar } from 'component'
 
 const Home = () => {
   const user = {
+    $id: '1',
     firstname: 'Joe',
     lastname: 'Doe',
     email: 'john.doe@example.com',
+    userId: 'asdad21',
+    dwollaCustomerUrl: 'asdadad2d1',
+    dwollaCustomerId: 'asdad21',
+    address: '123 Main St',
+    city: 'New York',
+    state: 'NY',
+    postalCode: '10001',
+    dateOfBirth: '01/01/1990',
+    ssn: '123-45-6789',
   }
+
+  const _account = [
+    {
+      $id: '1',
+      accountId: '**** **** **** 1234',
+      bankId: 'boa',
+      accessToken: 'asdadad2d1',
+      fundingSourceUrl: 'USD',
+      userId: 'asdad21',
+      sharableId: 'asda-1d1-123123',
+    },
+  ]
 
   return (
     <section className='home'>
@@ -15,6 +37,7 @@ const Home = () => {
           <BalanceBox account={[]} bank={1} totalCurrentBalance={2356} />
         </header>
       </div>
+      <RightSidebar user={user} bank={_account} transaction={[]} />
     </section>
   )
 }
