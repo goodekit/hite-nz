498c840fef4cf64796e4d8fed63f1e931e67a10d
bally <bally.lomibao@howickltd.com>
2024-05-15 23:56:02 +1200

    cleanup

--- component/index.ts
+++ component/index.ts
@@ -1,8 +1,15 @@
 export * from './header'
 export * from './ui/button'
-
+// :animate
 export { default as AnimateCounter } from './animate/counter'
+// :balance
 export { default as BalanceBox } from './balance/balance-box'
+// :header
 export { default as HeaderBox } from './header/header-box'
+// :chart
 export { default as DoughnutChart } from './chart/doughnut'
+// :sidebar
 export { default as Sidebar } from './sidebar/sidebar'
+export { default as RightSidebar } from './sidebar/right'
+// :nav
+export { default as MobileNav } from './nav/mobile'
