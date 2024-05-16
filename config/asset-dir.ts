4c196fcfeb7a761bb02fc634c305ce12e4dd18c4
bally <bally.lomibao@howickltd.com>
2024-05-16 23:31:03 +1200

    ```text feat: Add new payment method icons and image
    New payment method icons (PAY_PASS and MASTERCARD) and an image (LINES) have been added to the asset directory. These additions enhance the user interface by providing visually appealing representations of payment methods.

--- config/asset-dir.ts
+++ config/asset-dir.ts
@@ -2,4 +2,7 @@ export const ASSET = {
   ICON: '/asset/icon/hitenz.ico',
   OPEN_HAMBURGER: '/asset/svg/hamburger.svg',
   EXPAND: '/asset/svg/plus.svg',
+  PAY_PASS: '/asset/svg/paypass.svg',
+  MASTERCARD: '/asset/svg/mastercard.svg',
+  LINES: '/asset/image/lines.png',
 }
