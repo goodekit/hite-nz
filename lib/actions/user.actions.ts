94ed68715a5d4fe77e3058a6983a1c61ff14b1ac
bally <bally.lomibao@howickltd.com>
2024-05-19 03:54:56 +1200

    feat: Update user actions to include sign-up, sign-in, and sign-out functionality
    This commit updates the user.actions.ts file to include new functions for sign-up, sign-in, and sign-out functionality. The signIn function now handles creating a session and setting the appwrite-session cookie. The signUp function creates a new user account and session. The signOut function deletes the appwrite-session cookie and deletes the current session. These changes improve the authentication and user management capabilities of the application.

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,3 +1,80 @@
 'use server'
 
-// export const signIn = async (req, res) => {}
+import { ID, Query } from 'node-appwrite'
+import { createAdminClient, createSessionClient } from 'lib/server/appwrite'
+import { cookies } from 'next/headers'
+import { parseStringify } from 'lib/utils'
+
+const {
+  APPWRITE_DATABASE_ID: DATABASE_ID,
+  APPWRITE_USER_COLLECTION_ID: USER_COLLECTION_ID,
+  APPWRITE_BANK_COLLECTION_ID: BANK_COLLECTION_ID,
+} = process.env
+
+export const getUserInfo = async ({ userId }: getUserInfoProps) => {
+  try {
+    const { database } = await createAdminClient()
+
+    const user = await database.listDocuments(DATABASE_ID!, USER_COLLECTION_ID!, [Query.equal('userId', [userId])])
+
+    return parseStringify(user.documents[0])
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const signIn = async ({ email, password }: signInProps) => {
+  try {
+    const { account } = await createAdminClient()
+    const session = await account.createEmailPasswordSession(email, password)
+
+    cookies().set('appwrite-session', session.secret, {
+      path: '/',
+      httpOnly: true,
+      sameSite: 'strict',
+      secure: true,
+    })
+
+    const user = await getUserInfo({ userId: session.userId })
+
+    return parseStringify(session)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export const signUp = async (userData: SignUpParams) => {
+  try {
+    const { email, password, firstname, lastname } = userData
+    const name = `${firstname} ${lastname}`
+    const { account } = await createAdminClient()
+
+    const newUserAccount = await account.create(ID.unique(), email, password, name)
+    const session = await account.createEmailPasswordSession(email, password)
+
+    return parseStringify(newUserAccount)
+  } catch (error) {}
+}
+
+export async function getLoggedInUser() {
+  try {
+    const { account } = await createSessionClient()
+    const user = await account.get()
+
+    return parseStringify(user)
+  } catch (error) {
+    return null
+  }
+}
+
+export async function signOut() {
+  try {
+    const { account } = await createSessionClient()
+
+    cookies().delete('appwrite-session')
+
+    await account.deleteSession('current')
+  } catch (error) {
+    return null
+  }
+}
