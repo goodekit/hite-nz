7769a1230bf3b87788c830801c3242a15110a3c7
bally <bally.lomibao@howickltd.com>
2024-05-19 16:16:53 +1200

    feat: Add lazy loading for images in auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,19 @@
+import Image from 'next/image'
+import { ASSET } from 'config'
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  return <main>{children}</main>
+  return (
+    <main className='flex min-h-screen w-full justify-between font-inter'>
+      {children}{' '}
+      <div className='auth-asset'>
+        <div>
+          <Image src={ASSET.AUTH_IMAGE} alt='auth-image' fill className='object-cover h-full' />
+        </div>
+      </div>
+    </main>
+  )
 }
