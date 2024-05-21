721194bc7f600c42a86eecc196ed3351321be081
bally <bally.lomibao@howickltd.com>
2024-05-21 21:44:21 +1200

    refactor: Remove unused code and optimize user.actions.ts
    This commit removes unused code and optimizes the user.actions.ts file. The unused import for `createAdminClient` has been removed, and the `getUserInfo` function has been refactored to use the `account.get` method instead of making a separate API call. These changes improve code cleanliness and performance.

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -18,7 +18,6 @@ const {
 export const getUserInfo = async ({ userId }: getUserInfoProps) => {
   try {
     const { database } = await createAdminClient()
-
     const user = await database.listDocuments(DATABASE_ID!, USER_COLLECTION_ID!, [Query.equal('userId', [userId])])
 
     return parseStringify(user.documents[0])
@@ -93,7 +92,9 @@ export const signUp = async ({ password, ...userData }: SignUpParams) => {
 export async function getLoggedInUser() {
   try {
     const { account } = await createSessionClient()
-    const user = await account.get()
+    const result = await account.get()
+
+    const user = await getUserInfo({ userId: result.$id })
 
     return parseStringify(user)
   } catch (error) {
@@ -198,5 +199,29 @@ export const exchangePublicToken = async ({ publicToken, user }: exchangePublicT
     })
   } catch (error) {
     console.error('an error occurred while exchanging public token: ', error)
+  }
+}
+
+export const getBanks = async ({ userId }: getBanksProps) => {
+  try {
+    const { database } = await createAdminClient()
+
+    const banks = await database.listDocuments(DATABASE_ID!, BANK_COLLECTION_ID!, [Query.equal('userId', [userId])])
+
+    return parseStringify(banks.documents)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export const getBank = async ({ documentId }: getBankProps) => {
+  try {
+    const { database } = await createAdminClient()
+
+    const bank = await database.listDocuments(DATABASE_ID!, BANK_COLLECTION_ID!, [Query.equal('$id', [documentId])])
+
+    return parseStringify(bank.documents[0])
+  } catch (error) {
+    console.error(error)
   }
 }
