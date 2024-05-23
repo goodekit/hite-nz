7cb7523028c19bd1b971cbdec32d718680c3507f
ballyalley-o <bally.lomibao@howickltd.com>
2024-05-23 23:50:13 +1200

    feat: Update RecentTransaction component to use correct prop name
    This commit updates the RecentTransaction component in the Home page to use the correct prop name "transactions" instead of "transaction". This change ensures consistency with the RecentTransactionProps interface and improves code readability.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { BalanceBox, HeaderBox, RecentTransaction, RightSidebar } from 'component'
 import { getLoggedInUser } from 'lib/actions/user.actions'
 import { getAccounts, getAccount } from 'lib/actions/bank.actions'
@@ -5,7 +6,7 @@ import { getAccounts, getAccount } from 'lib/actions/bank.actions'
 const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
   const currentPage = Number(page as string) || 1
   const loggedInUser = await getLoggedInUser()
-  const accounts = await getAccounts({ userId: loggedInUser?.$id })
+  const accounts = await getAccounts({ userId: loggedInUser.$id })
 
   if (!accounts) return
 
@@ -21,8 +22,7 @@ const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
           <HeaderBox type='greeting' title='Welcome' user={loggedInUser?.firstName || 'Guest'} subtext='this is a description' />
           <BalanceBox account={accountsData} bank={accounts?.totalBanks} totalCurrentBalance={accounts?.totalCurrentBalance} />
         </header>
-
-        <RecentTransaction account={accountsData} transaction={account?.transactions} appwriteItemId={appwriteItemId} page={currentPage} />
+        <RecentTransaction account={accountsData} transactions={account?.transactions} appwriteItemId={appwriteItemId} page={currentPage} />
       </div>
       <RightSidebar user={loggedInUser} bank={accountsData?.slice(0, 2)} transaction={account?.transactions} />
     </section>
