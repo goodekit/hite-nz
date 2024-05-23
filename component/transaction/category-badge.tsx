05e4941e43566a2d547e30fa241d0b5207899eb9
ballyalley-o <bally.lomibao@howickltd.com>
2024-05-23 23:50:22 +1200

    feat: Add CategoryBadge component for displaying transaction category badge

---
+++ component/transaction/category-badge.tsx
@@ -0,0 +1,16 @@
+import { cn } from 'lib/utils'
+import { transactionCategoryStyles } from 'constant'
+
+const CategoryBadge = ({ category }: CategoryBadgeProps) => {
+  const { borderColor, backgroundColor, textColor, chipBackgroundColor } =
+    transactionCategoryStyles[category as keyof typeof transactionCategoryStyles] || transactionCategoryStyles.default
+
+  return (
+    <div className={cn('category-badge', borderColor, chipBackgroundColor)}>
+      <div className={cn('size-2 rounded-full', backgroundColor)} />
+      <p className={cn('text-[12px] font-medium', textColor)}>{category}</p>
+    </div>
+  )
+}
+
+export default CategoryBadge
--- component/transaction/index.ts
+++ component/transaction/index.ts
@@ -1,2 +1,3 @@
 export { default as RecentTransaction } from './recent'
 export { default as TableTransaction } from './table'
+export { default as CategoryBadge } from './category-badge'
