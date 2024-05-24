07926d575497d609b356455db28842fbcd7f264b
ballyalley-o <bally.lomibao@howickltd.com>
2024-05-24 23:53:21 +1200

    Update bank.actions.ts to import dependencies from the correct paths

--- lib/actions/bank.actions.ts
+++ lib/actions/bank.actions.ts
@@ -1,8 +1,10 @@
 'use server'
 
 import { ACHClass, CountryCode, TransferAuthorizationCreateRequest, TransferCreateRequest, TransferNetwork, TransferType } from 'plaid'
+
 import { plaidClient } from 'lib/plaid'
-import { parseStringify } from 'lib/utils'
+import { parseStringify } from '../utils'
+
 import { getTransactionsByBankId } from './transaction.actions'
 import { getBanks, getBank } from './user.actions'
 
@@ -36,7 +38,7 @@ export const getAccounts = async ({ userId }: getAccountsProps) => {
           type: accountData.type as string,
           subtype: accountData.subtype! as string,
           appwriteItemId: bank.$id,
-          sharaebleId: bank.shareableId,
+          shareableId: bank.shareableId,
         }
 
         return account
@@ -66,6 +68,7 @@ export const getAccount = async ({ appwriteItemId }: getAccountProps) => {
     })
     const accountData = accountsResponse.data.accounts[0]
 
+    // get transfer transactions from appwrite
     const transferTransactionsData = await getTransactionsByBankId({
       bankId: bank.$id,
     })
@@ -163,5 +166,39 @@ export const getTransactions = async ({ accessToken }: getTransactionsProps) => 
     return parseStringify(transactions)
   } catch (error) {
     console.error('An error occurred while getting the accounts:', error)
+  }
+}
+
+// Create Transfer
+export const createTransfer = async () => {
+  const transferAuthRequest: TransferAuthorizationCreateRequest = {
+    access_token: 'access-sandbox-cddd20c1-5ba8-4193-89f9-3a0b91034c25',
+    account_id: 'Zl8GWV1jqdTgjoKnxQn1HBxxVBanm5FxZpnQk',
+    funding_account_id: '442d857f-fe69-4de2-a550-0c19dc4af467',
+    type: 'credit' as TransferType,
+    network: 'ach' as TransferNetwork,
+    amount: '10.00',
+    ach_class: 'ppd' as ACHClass,
+    user: {
+      legal_name: 'Anne Charleston',
+    },
+  }
+  try {
+    const transferAuthResponse = await plaidClient.transferAuthorizationCreate(transferAuthRequest)
+    const authorizationId = transferAuthResponse.data.authorization.id
+
+    const transferCreateRequest: TransferCreateRequest = {
+      access_token: 'access-sandbox-cddd20c1-5ba8-4193-89f9-3a0b91034c25',
+      account_id: 'Zl8GWV1jqdTgjoKnxQn1HBxxVBanm5FxZpnQk',
+      description: 'payment',
+      authorization_id: authorizationId,
+    }
+
+    const responseCreateResponse = await plaidClient.transferCreate(transferCreateRequest)
+
+    const transfer = responseCreateResponse.data.transfer
+    return parseStringify(transfer)
+  } catch (error) {
+    console.error('An error occurred while creating transfer authorization:', error)
   }
 }
