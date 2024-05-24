00ea6e0242b77525fb454a3e3df5db3578aba752
ballyalley-o <bally.lomibao@howickltd.com>
2024-05-24 23:49:38 +1200

    Update MyAccount page to fetch and display user accounts
    This commit updates the MyAccount page to fetch and display user accounts. It adds the necessary imports for the HeaderBox component and the getLoggedInUser and getAccounts functions. The page now makes an asynchronous call to get the logged-in user and their accounts, and renders them in a section with a title and subtext. It also includes a section for displaying the user's cards, which is currently empty. This change enhances the functionality of the MyAccount page by providing the user with information about their accounts and settings.

--- app/(root)/my-account/page.tsx
+++ app/(root)/my-account/page.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import { HeaderBox } from 'component'
+import { getLoggedInUser } from 'lib'
+import { getAccounts } from 'lib/actions'
 
-const MyAccount = () => {
-  return <div>MyAccount</div>
+const MyAccount = async () => {
+  const loggedIn = await getLoggedInUser()
+  const accounts = await getAccounts({
+    userId: loggedIn.$id,
+  })
+  return (
+    <section className='flex'>
+      <div className='my-account'>
+        <HeaderBox title='My Accounts' subtext='Manage your account and settings' />
+        <div className='space-y-4'>
+          <div className='header-2'>
+            <h2>Your Cards</h2>
+          </div>
+          <div className='flex flex-wrap gap-6'></div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default MyAccount
