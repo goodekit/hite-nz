beb4f7e0d84112cd5cc92c812ad13dce022212eb
ballyalley-o <bally.lomibao@howickltd.com>
2024-05-25 22:16:55 +1200

    feat: Update Transfer page to fetch and display user accounts
    This commit updates the Transfer page to fetch and display user accounts. It adds the necessary imports for the HeaderBox component and the getLoggedInUser and getAccounts functions. The page now makes an asynchronous call to get the logged-in user and their accounts, and renders them in a section with a title and subtext. It also includes a PaymentTransferForm component that allows the user to provide specific details for the payment transfer. This change enhances the functionality of the Transfer page by providing the user with information about their accounts and improving the user experience for making transfers.

--- app/(root)/transfer/page.tsx
+++ app/(root)/transfer/page.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { HeaderBox, PaymentTransferForm } from 'component'
+import { getLoggedInUser, getAccounts } from 'lib/actions'
 
-const Transfer = () => {
-  return <div>Transfer</div>
+const Transfer = async () => {
+  const loggedInUser = await getLoggedInUser()
+  const accounts = await getAccounts({ userId: loggedInUser.$id })
+
+  if (!accounts) return
+
+  const accountsData = accounts?.data
+
+  return (
+    <section className='payment-transfer'>
+      <HeaderBox title='Payment Transfer' subtext='Please provide any specific details for the payment transfer.' />
+
+      <section className='size-full pt-5'>
+        <PaymentTransferForm accounts={accountsData} />
+      </section>
+    </section>
+  )
 }
 
 export default Transfer
