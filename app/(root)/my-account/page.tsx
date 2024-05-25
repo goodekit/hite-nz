90e230cef0996f8432d04fbebeba0c1f2e92fda8
ballyalley-o <bally.lomibao@howickltd.com>
2024-05-25 22:16:17 +1200

    Add BankCard component to MyAccount page
    This commit adds the BankCard component to the MyAccount page. The BankCard component is imported from the 'component' module and is used to display the user's cards. The component is rendered within a flex container, allowing for multiple cards to be displayed in a row with a gap of 6 units between them. Each card is associated with an account and includes the account owner's username. This enhancement improves the functionality of the MyAccount page by providing a visual representation of the user's cards.

--- app/(root)/my-account/page.tsx
+++ app/(root)/my-account/page.tsx
@@ -1,4 +1,4 @@
-import { HeaderBox } from 'component'
+import { BankCard, HeaderBox } from 'component'
 import { getLoggedInUser } from 'lib'
 import { getAccounts } from 'lib/actions'
 
@@ -15,7 +15,9 @@ const MyAccount = async () => {
           <div className='header-2'>
             <h2>Your Cards</h2>
           </div>
-          <div className='flex flex-wrap gap-6'></div>
+          <div className='flex flex-wrap gap-6'>
+            {accounts && accounts?.data.map((a: Account) => <BankCard key={accounts.id} account={a} username={loggedIn?.firstName} />)}
+          </div>
         </div>
       </div>
     </section>
